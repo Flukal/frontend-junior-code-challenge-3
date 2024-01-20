7d917d720ab2f05a2e14e63854e3af2342314cb5
Fulvio Calderone <fluviobass@gmail.com>
2024-01-20 23:23:57 +0100

    added tailwind module

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        '16': 'repeat(16, minmax(0, 1fr))',
+        '32': 'repeat(32, minmax(0, 1fr))',
+      }
+    }
+  }
+}
