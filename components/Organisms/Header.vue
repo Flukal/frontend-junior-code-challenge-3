8df5a4c14558684bf1bd45c0ca1d9b79261bc3eb
Fulvio Calderone <fluviobass@gmail.com>
2024-01-23 23:05:00 +0100

    added github icon and link navigation

--- components/Organisms/Header.vue
+++ components/Organisms/Header.vue
@@ -1,5 +1,6 @@
 <template>
-  <nav class="text-white bg-blue-200 py-4 pl-8 uppercase font-semibold">
+  <nav class="text-white bg-blue-200 py-4 px-8 uppercase font-semibold flex justify-between items-center">
     <NuxtLink to="/"><img src="../../assets/icon/logo.svg" alt="Logo" class="w-52" /></NuxtLink>
+    <NuxtLink to="https://github.com/Flukal/frontend-junior-code-challenge-3" class="ml-8" target="_blank"><img src="../../assets/icon/github.svg" alt="Logo" class="w-8" /></NuxtLink>
   </nav>
 </template>
